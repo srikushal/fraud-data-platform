e2cc64dbc961eb34403e96bcf61b56baec72f48a
Sri Kushal <srikushal@Sris-MacBook-Air.local>
2026-06-13 17:15:22 +0530

    Add data cleaning pipeline

--- scripts/load_data.py
+++ scripts/load_data.py
@@ -1,33 +1,34 @@
 import pandas as pd
 
-# file path
-file_path = "data/raw/creditcard.csv"
+# Read dataset
+df = pd.read_csv("data/raw/creditcard.csv")
 
-try:
-    # read csv file
-    df = pd.read_csv(file_path)
+print("Dataset loaded successfully!")
+print("-" * 40)
 
-    print("Dataset loaded successfully!")
-    print("-" * 40)
+# Check missing values
+print("Missing Values:")
+print(df.isnull().sum())
 
-    # rows and columns
-    print(f"Total Rows: {df.shape[0]}")
-    print(f"Total Columns: {df.shape[1]}")
+print("-" * 40)
 
-    print("-" * 40)
+# Check duplicate rows
+duplicate_count = df.duplicated().sum()
 
-    # column names
-    print("Column Names:")
-    print(df.columns.tolist())
+print(f"Duplicate Rows: {duplicate_count}")
 
-    print("-" * 40)
+print("-" * 40)
 
-    # first 5 rows
-    print("First 5 Rows:")
-    print(df.head())
+# Remove duplicates
+df = df.drop_duplicates()
 
-except FileNotFoundError:
-    print("File not found. Please check the file path.")
+print("After Removing Duplicates:")
 
-except Exception as e:
-    print("Error:", e)
+print(f"Total Rows: {df.shape[0]}")
+print(f"Total Columns: {df.shape[1]}")
+
+# Save cleaned dataset
+df.to_csv("data/clean/cleaned_creditcard.csv", index=False)
+
+print("-" * 40)
+print("Cleaned dataset saved successfully!")
